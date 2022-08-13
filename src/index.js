219a0b734ff8bbb9cabea636b1e4f3e7ef370c80
pablo.paparini.extern <pablo.paparini.extern@elinvar.de>
2022-08-13 19:56:35 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,107 @@
+import { RSocketClient, JsonSerializer, IdentitySerializer } from 'rsocket-core';
+import RSocketWebSocketClient from 'rsocket-websocket-client';
+
+// backend ws endpoint
+const wsURL = 'ws://localhost:6567/rsocket';
+var subscriptor;
+var timeOut = [];
+var providerRateDelay = 10
+// rsocket client
+const client = new RSocketClient({
+    serializers: {
+        data: JsonSerializer,
+        metadata: IdentitySerializer
+    },
+    setup: {
+        keepAlive: 60000,
+        lifetime: 180000,
+        dataMimeType: 'application/json',
+        metadataMimeType: 'message/x.rsocket.routing.v0',
+    },
+    transport: new RSocketWebSocketClient({
+        url: wsURL
+    })
+});
+
+// error handler
+const errorHanlder = (e) => console.log(e);
+// response handler
+const responseHanlder = (payload) => {
+    const li = document.createElement('li');
+    li.innerText = payload.data.rate;
+    li.classList.add('list-group-item', 'small');
+
+    if (payload.data.type == "rate"){
+        document.getElementById(payload.data.origin).innerHTML = payload.data.rate;
+        clearTimeout(timeOut[payload.data.origin]); 
+        timeOut[payload.data.origin] = setTimeout(resetCore, 1500, payload.data.origin);
+    } else {
+        let textArea = document.getElementById(payload.data.origin + "Log");
+        appendLog(textArea, payload.data.entry);
+    }
+
+    
+    subscriptor.request(1);
+    //if (payload.data.origin = "core")
+    //console.log(payload.data.origin);
+}
+
+const numberRequester = (socket) => {
+    socket.requestStream({
+        //data: value,
+        metadata: String.fromCharCode('logger.stream'.length) + 'logger.stream'
+    }).subscribe({
+        onError: errorHanlder,
+        onNext: responseHanlder,
+        onSubscribe: subscription => {
+                subscription.request(1); // set it to some max value
+                subscriptor = subscription
+        }
+    });
+
+}
+
+client.connect().then(sock => {
+    numberRequester(sock);
+    document.getElementById('startFlow').addEventListener('click', ({srcElement}) => {
+        sock.fireAndForget({
+            data: true,
+            metadata: String.fromCharCode('logger.start.flow'.length) + 'logger.start.flow'
+        });
+        
+    })
+    document.getElementById('startLogs').addEventListener('click', ({srcElement}) => {
+        sock.fireAndForget({
+            data: true,
+            metadata: String.fromCharCode('logger.start.logs'.length) + 'logger.start.logs'
+        });
+        
+    })
+    document.getElementById('prdp').addEventListener('click', ({srcElement}) => {
+        providerRateDelay+=5
+        sock.fireAndForget({
+            data: providerRateDelay,
+            metadata: String.fromCharCode('logger.provider.rate'.length) + 'logger.provider.rate'
+        });
+        document.getElementById('prdn').value = providerRateDelay;
+    })
+    document.getElementById('prdm').addEventListener('click', ({srcElement}) => {
+        providerRateDelay-=5
+        sock.fireAndForget({
+            data: providerRateDelay,
+            metadata: String.fromCharCode('logger.provider.rate'.length) + 'logger.provider.rate'
+        });
+        document.getElementById('prdn').value = providerRateDelay;
+    })
+}, errorHanlder);
+
+deSerializeMsg: ({ data, metadata }) => {
+    return {
+      data: data != null ? JSON.parse(data) : null,
+      metadata: metadata != null ? JSON.parse(metadata) : null
+    };
+  }
+
+  function resetCore(what){
+    document.getElementById(what).innerHTML = '0'
+  }
